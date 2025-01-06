690b57d945346db4f781de304693822b1fb27b09
Andrew Vasquez <andrewvasquez0113@gmail.com>
2025-01-05 22:24:33 -0800

    Clerk and Shadcn initialized with theme switching

--- app/root.tsx
+++ app/root.tsx
@@ -4,10 +4,18 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useRouteLoaderData,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
 
 import "./tailwind.css";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp } from "@clerk/remix";
+import { dark } from "@clerk/themes";
+import { themeSessionResolver } from "./sessions.server";
+import { ThemeProvider, useTheme } from "remix-themes";
+import clsx from "clsx";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,11 +28,26 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap",
+  },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const loader: LoaderFunction = (args) => {
+  return rootAuthLoader(args, async ({ request }) => {
+    const { getTheme } = await themeSessionResolver(request);
+    const { sessionId, userId, getToken } = request.auth;
+    // fetch data
+    return { theme: getTheme() };
+  });
+};
+
+export function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -32,7 +55,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -40,6 +63,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+export function AppWithProviders() {
+  const data = useLoaderData<typeof loader>();
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
 }
+
+// Use the dynamic theme selection in ClerkApp
+export default ClerkApp(AppWithProviders);
---
+++ app/routes/signin.tsx
@@ -0,0 +1,26 @@
+import { SignIn } from "@clerk/remix";
+import { MetaFunction, useLoaderData } from "@remix-run/react";
+import React from "react";
+import { useTheme } from "remix-themes";
+import { dark } from "@clerk/themes";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Sign in to Chat" },
+    { name: "description", content: "Sign in to Chat!" },
+  ];
+};
+export default function Signin() {
+  const [theme] = useTheme();
+  console.log(theme);
+  return (
+    <div className="flex flex-col items-center justify-around h-screen">
+      <h1 className="text-5xl font-instrument font-bold">Chat</h1>
+      <div className="w-fit dark:border-white border-black border-[3px] rounded-[14px]">
+        <SignIn
+          appearance={{ baseTheme: theme === "dark" ? dark : undefined }}
+        />
+      </div>
+    </div>
+  );
+}
